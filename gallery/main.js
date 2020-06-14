d332684efa130bd7779b0cdd497a1393b018b0f4
Wayne Stedman <waynestedman@users.noreply.github.com>
2020-06-14 15:48:29 -0700

    prev & next working

--- gallery/main.js
+++ gallery/main.js
@@ -1,4 +1,5 @@
 var target = document.getElementById('gallery');
+var slideIndex = 1;
 
 function galleryMode() {
   var galleryItems = document.getElementsByClassName('gallery-item');
@@ -12,42 +13,36 @@ function galleryMode() {
   }
 }
 
-;
+; // Next/previous controls
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('gallery-item');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+}
 
 function slideshowMode() {
-  var slideIndex = 1;
   target.style.display = 'block';
   target.classList.add('slideshow');
   target.classList.remove('gallery');
-  showSlides(slideIndex); // Next/previous controls
-
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  } // Thumbnail image controls
-
-
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName('gallery-item');
-
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-    }
-
-    slides[slideIndex - 1].style.display = 'block';
-  }
+  showSlides(slideIndex);
 }
 
 ;
