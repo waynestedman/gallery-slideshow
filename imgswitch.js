11f1515da1e765b37ade64080326019afd1f08ce
Wayne Stedman <waynestedman@users.noreply.github.com>
2020-06-14 17:00:44 -0700

    node script clean up

--- imgswitch.js
+++ imgswitch.js
@@ -2,15 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 /* create _assets folder */
-// fs.mkdir(path.join(__dirname, '_assets'), (err) => {
 fs.mkdir('_assets', (err) => {
   if (err) {
     return console.log(err);
   }
   console.log('Directory created successfully!');
 });
-
-/*  */
 
 /* Transform png into jpg & Copy file into _assets dir */
 fs.copyFile('./src/cat.png', './_assets/cat.jpg', (err) => {
